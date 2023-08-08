e8f59198ae16f3c1b05b4b9690ab1db711adfaa8
kevin <kmathew@peloton-technologies.com>
2023-08-08 01:27:03 -0700

    setting up mantine

---
+++ frontend/app/emotion.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { CacheProvider } from '@emotion/react';
+import { useEmotionCache, MantineProvider } from '@mantine/core';
+import { useServerInsertedHTML } from 'next/navigation';
+
+export default function RootStyleRegistry({ children }: { children: React.ReactNode }) {
+    const cache = useEmotionCache();
+    cache.compat = true;
+
+    useServerInsertedHTML(() => (
+        <style
+            data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(' ')}`}
+            dangerouslySetInnerHTML={{
+                __html: Object.values(cache.inserted).join(' '),
+            }}
+        />
+    ));
+
+    return (
+        <CacheProvider value={cache}>
+            <MantineProvider withGlobalStyles withNormalizeCSS
+                theme={{
+                    fontFamily: "Satoshi, sans-serif",
+                    colors: {
+                        blue: ["#f3f0ff", "#e5dbff", "#d0bfff", "#b197fc", "#9775fa", "#845ef7", "#7950f2", "#7048e8", "#6741d9", "#5f3dc4"],
+                    }
+                }}
+            >
+                {children}
+            </MantineProvider>
+        </CacheProvider>
+    );
+}
